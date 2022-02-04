aae7a1ef37be6219075eb71320244eb34197cc87
Lawrence Jones <git@lwjon.es>
2022-02-03 17:02:12 -0800

    Pull js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,53 +3,35 @@ const countryNameEl = document.getElementById("country_name");
 const countryListEl = document.getElementById("country_list");
 
 
-// Handles asynchronous fetch requests and then calls the respective method to handle the data once its received 
+// Handles asynchronous fetch requests and then calls the respective method to handle the data once its received
 function network_manager(request) {
-    
+
     const info_request = request.split("?")[0].trim();
     const parameter = request.split("?")[1];
-    
+
     let apiUrl = null;
     let base = null;
 
     // Sets the apiUrl depending on which data is being fetched
     switch(info_request){
-        case "latest-rates":
-            // This address is for the latest exchange rate data from OpenExchange's API
-            // Format latest-rates?USD
-
-            // If no base country is passed in it will default to USD
-            base = !parameter ? "USD" : parameter.trim();
-
-            apiUrl = "https://openexchangerates.org/api/latest.json?base=" + base + "&app_id=801157a5b7c8404aacf491bfd6a1b2f4";
-            break;
-
-        case "rate-history":
-            // This address is for the exchange rate data on a past date from OpenExchange's API
-            // Format rate-history?USD&YYYY-MM-DD
-            
-            base = parameter.split("&")[0].trim();
-            const date = parameter.split("&")[1].trim();
-
-            apiUrl = "https://openexchangerates.org/api/historical/" + date + ".json?" + "base=" + base + "&app_id=801157a5b7c8404aacf491bfd6a1b2f4";
-            break;
-
-        case "currencies":
-            // This address is for an array of all the available countries and their respective currency codes (e.g. USD) from OpenExchange's API
-            
-            apiUrl = "https://openexchangerates.org/api/currencies.json";
-            break;
 
         case "countries":
             // This address is for an array of all the available countries, their full names, country codes amd briefing URL from TravelBriefing's API
-            
+
             apiUrl = "https://travelbriefing.org/countries?format=json";
             break;
 
         case "country-briefing":
             // This address is for an object of the breifing data from TravelBriefing's API for the country that was passed in with the request
-            
+
             apiUrl = parameter + "?format=json";
+            break;
+
+        case "weather":
+            // This address is for an object to retrieve the current weather for the lat/long
+            // Example format: https://fcc-weather-api.glitch.me/api/current?lat=25.9&lon=50.6
+
+            apiUrl = "https://fcc-weather-api.glitch.me/api/current?" + parameter;
             break;
 
         default:
@@ -87,7 +69,7 @@ function network_manager(request) {
             // If the response from the fetch is anything but OK.
 
             alert("A network error has occured!\n\n" + response);
-        } 
+        }
     });
 }
 
@@ -117,9 +99,9 @@ function currency_history_received(history_data) {
 }
 
 function countries_received(country_data) {
-   
+
     // When the data is recieved from the network call it saves it in the global variable and saves it to local storage for later use, the populates the drop-down
-   
+
     country_list = country_data;
     window.localStorage.setItem("countries", JSON.stringify(country_data));
     populate_country_list();
@@ -133,7 +115,7 @@ function brief_received(brief_data) {
 
     const mapEl = document.getElementById("map");
     const title = document.getElementById("title_text");
-    
+
     // Set title
     title.textContent = brief_data.names.full;
 
@@ -143,9 +125,9 @@ function brief_received(brief_data) {
     const zoom = parseInt(brief_data.maps.zoom);
 
     // Converts the quadrant of the lat/long from +/- to the respective compass heading
-    let map_lat = lat < 0 ? "S" + Math.abs(lat).toString() : "N" + lat.toString(); 
+    let map_lat = lat < 0 ? "S" + Math.abs(lat).toString() : "N" + lat.toString();
     let map_lng = lng < 0 ? "W" + Math.abs(lng).toString() : "E" + lng.toString();
-   
+
     // Changes the map's source URL to show the respective country
     mapEl.src = "https://maps.google.com/maps?q="+ map_lat + map_lng + "&t=&z=" + zoom + "&ie=UTF8&iwloc=&output=embed";
 
@@ -164,13 +146,13 @@ function get_countries() {
         country_list = list;
         populate_country_list();
     } else {
-        network_manager("countries");        
+        network_manager("countries");
     }
 }
 
-function populate_country_list() {    
+function populate_country_list() {
     // Dynamically adds an option element with each country's name to the drop down list so the country can be selected
-    
+
     for(let i=0; i < country_list.length; i++) {
         const optionEl = document.createElement("option");
         optionEl.setAttribute("value", country_list[i].name);
@@ -180,10 +162,10 @@ function populate_country_list() {
 
 function getBriefingUrl(selection) {
     // returns the url of the select country, if there is no URL it will return a null
-    
+
     let inList = null;
-    
-    country_list.forEach(element => {    
+
+    country_list.forEach(element => {
         if(element.name === selection) {
             inList = element.url;
         }
@@ -197,12 +179,12 @@ function getBriefingUrl(selection) {
 // Eevent listeners that are initiated on page load
 countryNameEl.addEventListener('change', function(event) {
     // Triggers when the user selects a country from the drop-down list
-    
+
     // Gets the url of the briefing for the selected country
     const brief_url = getBriefingUrl(event.target.value);
 
     // Makes sure there is a url and sends the request to the network manager
-    if(brief_url) { 
+    if(brief_url) {
         network_manager("country-briefing?" + brief_url);
         countryNameEl.value = "";
     } else {
@@ -210,7 +192,16 @@ countryNameEl.addEventListener('change', function(event) {
         countryNameEl.focus();
     }
 });
-   
+<<<<<<< Updated upstream
+
 
 // Loads the countries into the drop down on page load
 get_countries();
+=======
+
+// Loads the countries into the drop down on page load
+get_countries();
+
+
+network_manager("weather?lat=25.9&lon=50.6")
+>>>>>>> Stashed changes
